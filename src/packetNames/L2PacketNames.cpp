0576f2b79b911c97381a5cc5d8943416f599bb22
alexey.min <alexey.min@70e4b00a-f7aa-11dd-9fdb-3debd5008f28>
2009-06-20 14:56:38 +0000

    Adding packet names for Gracia Final... Packet names rewrite, now names are returned by l2 version

--- src/packetNames/L2PacketNames.cpp
+++ src/packetNames/L2PacketNames.cpp
@@ -1,23 +1,82 @@
 #include "stdafx.h"
+#include "../L2_versions.h"
 #include "L2PacketNames.h"
 
+// prototypes for internal functions
 void L2PacketNames_InitServer();
 void L2PacketNames_InitClient();
+
+void L2Packets_GetL2PacketName_fromServer(
+	unsigned char   opcode1,
+	unsigned short  opcode2,
+	char           *outPacketName,
+	unsigned int    cchMax
+);
+
+void L2Packets_GetL2PacketName_fromClient(
+	unsigned char   opcode1,
+	unsigned short  opcode2,
+	char           *outPacketName,
+	unsigned int    cchMax
+);
+
+// Gracia Final
+void L2PacketNames_InitClient_t23();
+void L2PacketNames_InitServer_t23();
+
+void L2Packets_GetL2PacketName_fromClient_t23(
+	unsigned char   opcode1,
+	unsigned short  opcode2,
+	unsigned short  opcode3,
+	char           *outPacketName,
+	unsigned int cchMax );
+
+void L2Packets_GetL2PacketName_fromServer_t23(
+	unsigned char opcode1,
+	unsigned short opcode2,
+	char *outPacketName,
+	unsigned int cchMax );
 
 void L2PacketNames_Init()
 {
 	L2PacketNames_InitServer();
 	L2PacketNames_InitClient();
+	L2PacketNames_InitClient_t23();
+	L2PacketNames_InitServer_t23();
 }
 
-void L2Packets_GetL2PacketName( bool fromServer,
-	unsigned char opcode1, unsigned short opcode2,
-	char *outPacketName, unsigned int cchMax )
+void L2Packets_GetL2PacketName(
+	L2_VERSION       l2_version,
+	bool             fromServer,
+	unsigned char    opcode1,
+	unsigned short   opcode2,
+	unsigned short   opcode3,
+	char            *outPacketName,
+	unsigned int     cchMax
+)
 {
-	if( fromServer )
-		L2Packets_GetL2PacketName_fromServer( opcode1, opcode2, outPacketName, cchMax );
-	else
-		L2Packets_GetL2PacketName_fromClient( opcode1, opcode2, outPacketName, cchMax );
+	switch( l2_version )
+	{
+	case L2_VERSION_T1:
+	case L2_VERSION_T2:
+	case L2_VERSION_T22:
+		{
+			// Kamael -  Gracia Part 2 have same packets (only adds, no changes/deletes)
+			if( fromServer )
+				L2Packets_GetL2PacketName_fromServer( opcode1, opcode2, outPacketName, cchMax );
+			else
+				L2Packets_GetL2PacketName_fromClient( opcode1, opcode2, outPacketName, cchMax );
+		} break;
+	case L2_VERSION_T23:
+		{
+			// Gracia Final has too many differences...
+			if( fromServer )
+				L2Packets_GetL2PacketName_fromServer_t23( opcode1, opcode2, outPacketName, cchMax );
+			else
+				L2Packets_GetL2PacketName_fromClient_t23( opcode1, opcode2, opcode3, outPacketName, cchMax );
+		} break;
+	}
+
 	outPacketName[cchMax] = 0;
 }
 
--- src/packetNames/L2PacketNames.h
+++ src/packetNames/L2PacketNames.h
@@ -11,35 +11,20 @@ void L2PacketNames_Init();
 /** Get packet name (string) by packet opcode
 * \param fromServer - must be true for Server->Client packet
 * \param opcode1 - main packet opcode
-* \param opcode2 - extended packet opcode. Used only for packets S->C 0xFE and C->S 0xD0
+* \param opcode2 - extended packet opcode. Used only for packets S->C FE and C->S D0
+* \param opcode3 - extended packet opcode. Used only in Gracia Final for packets S->C FE:?? and C->S D0:51
 * \param outPacketName - pointer to buffer to receive packet name
 * \param cchMax - buffer size
 * \return resulting string in outPacketName
 */
-void L2Packets_GetL2PacketName( bool fromServer,
-	unsigned char opcode1, unsigned short opcode2,
-	char *outPacketName, unsigned int cchMax );
-
-/** Get packet name (string) by packet opcode for Server->Client packets
-* \param opcode1 - main packet opcode
-* \param opcode2 - extended packet opcode. Used only for packets S->C 0xFE
-* \param outPacketName - pointer to buffer to receive packet name
-* \param cchMax - buffer size
-* \return resulting string in outPacketName
-*/
-void L2Packets_GetL2PacketName_fromServer(
-	unsigned char opcode1, unsigned short opcode2,
-	char *outPacketName, unsigned int cchMax );
-
-/** Get packet name (string) by packet opcode for Client->Server packets
-* \param opcode1 - main packet opcode
-* \param opcode2 - extended packet opcode. Used only for packets C->S 0xD0
-* \param outPacketName - pointer to buffer to receive packet name
-* \param cchMax - buffer size
-* \return resulting string in outPacketName
-*/
-void L2Packets_GetL2PacketName_fromClient(
-	unsigned char opcode1, unsigned short opcode2,
-	char *outPacketName, unsigned int cchMax );
+void L2Packets_GetL2PacketName(
+	L2_VERSION       l2_version,
+	bool             fromServer,
+	unsigned char    opcode1,
+	unsigned short   opcode2,
+	unsigned short   opcode3,
+	char            *outPacketName,
+	unsigned int     cchMax
+);
 
 #endif
