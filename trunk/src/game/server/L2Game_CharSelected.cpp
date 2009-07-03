47427117e29c4cb34ae65f58d4bf151ced74278e
alexey.min <alexey.min@70e4b00a-f7aa-11dd-9fdb-3debd5008f28>
2009-07-03 22:20:51 +0000

    Parsing of KeyPacket, CharSelected Fix issue 1 xD

--- trunk/src/game/server/L2Game_CharSelected.cpp
+++ trunk/src/game/server/L2Game_CharSelected.cpp
@@ -1,2 +1,124 @@
 #include "stdafx.h"
 #include "L2Game_CharSelected.h"
+
+L2Game_CharSelected::L2Game_CharSelected()
+{
+	_initNull();
+}
+
+L2Game_CharSelected::L2Game_CharSelected( const unsigned char *bytes, unsigned int length )
+{
+	_initNull();
+	setBytes( bytes, length );
+}
+
+bool L2Game_CharSelected::parse( L2_VERSION ver /*= L2_VERSION_T1*/ )
+{
+	UNREFERENCED_PARAMETER(ver);
+	if( !this->canReadBytes( 200 ) ) return false; // must be at least 200 bytes
+
+	readReset();
+	readUChar(); // 0x0b CharSelected
+
+	wcscpy( char_name, readUnicodeStringPtr() ); // char name
+	charId    = readUInt();                      // char id (objectId)
+	wcscpy( title, readUnicodeStringPtr() );     // title
+	sessionId = readUInt();                      // sessionId
+	clanId    = readUInt();                      // clanId
+	readD();                                     // 0x00 (maybe allyId?)
+	sex       = readD();
+	race      = readD();
+	classId   = readUInt();
+	isActive  = readD();
+
+	x         = readD();
+	y         = readD();
+	z         = readD();
+
+	curHp     = readF();
+	curMp     = readF();
+	sp        = readUInt();
+	exp       = readUInt64();
+	level     = readD();
+	karma     = readD();
+	PK_kills  = readD();
+
+	s_INT = readD();
+	s_STR = readD();
+	s_CON = readD();
+	s_MEN = readD();
+	s_DEX = readD();
+	s_WIT = readD();
+
+	readD(); // writeD( 0x0451 ); //?
+	readD(); // writeD( 0x00 ); // some constants
+
+	readD(); // writeD( classId ); // classId already read?
+
+	//writeD(0x00); // 4 x 0x00
+	//writeD(0x00);
+	//writeD(0x00);
+	//writeD(0x00); 
+	readD(); readD(); readD(); readD();
+
+	//writeB(new byte[64]); // some 64 bytes
+	int i;
+	for( i=0; i<16; i++ ) readD(); // read 4 x 16 = 64 bytes
+
+	opcodeObfuscatorSeed = readUInt();
+	return true;
+}
+
+bool L2Game_CharSelected::create( L2_VERSION ver /*= L2_VERSION_T1*/ )
+{
+	UNREFERENCED_PARAMETER(ver);
+	writeReset();
+	writeC(0x0b);
+
+	writeS( char_name );
+	writeD( charId );
+	writeS( title );
+	writeD( sessionId );
+	writeD( clanId );
+	writeD( 0x00 );  // maybe allyId?
+	writeD( sex );
+	writeD( race );
+	writeD( classId );
+	writeD( isActive ); // active ??
+
+	writeD( x );
+	writeD( y );
+	writeD( z );
+
+	writeF( curHp );
+	writeF( curMp );
+	writeD( sp );
+	writeQ( exp );
+	writeD( level );
+	writeD( karma );
+	writeD( PK_kills );
+	writeD( s_INT );
+	writeD( s_STR );
+	writeD( s_CON );
+	writeD( s_MEN );
+	writeD( s_DEX );
+	writeD( s_WIT );
+
+	writeD( 0x0451 ); //?
+	writeD( 0x00 ); // some constants
+
+	writeD( classId );
+
+	writeD(0x00); // 4 x 0x00
+	writeD(0x00);
+	writeD(0x00);
+	writeD(0x00); 
+
+	//writeB(new byte[64]); // some 64 bytes
+	unsigned char *rndbuf = (unsigned char *)malloc( 64 );
+	writeBytes( rndbuf, 64 );
+	free( rndbuf );
+
+	writeD( opcodeObfuscatorSeed ); // writeD(0x00);
+	return true;
+}
--- trunk/src/game/server/L2Game_CharSelected.h
+++ trunk/src/game/server/L2Game_CharSelected.h
@@ -9,6 +9,35 @@ public:
 	L2Game_CharSelected();
 	L2Game_CharSelected( const unsigned char *bytes, unsigned int length );
 public:
+	virtual bool parse( L2_VERSION ver = L2_VERSION_T1 );
+	virtual bool create( L2_VERSION ver = L2_VERSION_T1 );
+public:
+	wchar_t char_name[128]; // char name
+	unsigned int charId; // char id (objectId)
+	wchar_t title[128];  // title (empty here)
+	unsigned int sessionId;  // sessionId
+	unsigned int clanId; // clan id
+	int sex; // sex
+	int race; // race 0x01 - elf
+	unsigned int classId;  // class Id
+	int isActive;  // is Active
+	int x;
+	int y;
+	int z;
+	double curHp; // currentHp
+	double curMp; // currentMp
+	unsigned int sp; // sp
+	unsigned long long int exp; // exp
+	int level; // level (64)
+	int karma; // karma
+	int PK_kills; // PK kills
+	int s_INT; // INT
+	int s_STR; // STR
+	int s_CON; // CON
+	int s_MEN; // MEN
+	int s_DEX; // DEX
+	int s_WIT; // WIT
+	unsigned int opcodeObfuscatorSeed; // opcode obfuscator seed
 };
 
 #endif
